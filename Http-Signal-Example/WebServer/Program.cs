5e70c970d95cf185b52743c675435fdab0aec49d
Rory Claasen <rory@xbox.com>
2022-07-12 17:16:52 +0100

    Fixes issues with code. Adds more info in readme

--- Http-Signal-Example/WebServer/Program.cs
+++ Http-Signal-Example/WebServer/Program.cs
@@ -1,11 +1,13 @@
 using System.Collections.Concurrent;
 using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
 var storage = new ConcurrentDictionary<string, List<object>>();
+
+app.MapGet("/", () => storage);
 
 app.MapGet("/{endpoint}", (string endpoint) => storage.ContainsKey(endpoint) ? storage[endpoint] : Enumerable.Empty<object>());
 
@@ -14,22 +16,24 @@ app.MapPost("/{endpoint}", async delegate (HttpContext context)
     string endpoint = context.Request.RouteValues["endpoint"]?.ToString() ?? "unknown";
     if (!storage.ContainsKey(endpoint))
     {
-        storage.GetOrAdd("endpoint", new List<object>());
+        storage.GetOrAdd(endpoint, new List<object>());
     }
 
-    using (StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8))
+    using (StreamReader reader = new(context.Request.Body, Encoding.UTF8))
     {
-        string body = await reader.ReadToEndAsync();
 
+        
         storage[endpoint].Add(new
         {
             Time = DateTime.UtcNow,
-            Path = context.Request.Path,
-            Query = context.Request.Query,
-            Body = body
+            Path = context.Request.Path.Value,
+            context.Request.Query,
+            Body = JsonSerializer.Deserialize(await reader.ReadToEndAsync(), typeof(object))
+
+
         });
 
-        return "You just posted";
+        return $"You just posted to {endpoint}";
     }
 });
 
--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # C++ Http signal example
 
+This is an example solution of how you could use a cheap http post to signal to a web server.
+
+## Usage
+
+1. Clone the repository
+
+   ```text
+   git clone --recursive https://github.com/roryclaasen/http-signal-example.git
+   ```
+
+2. Navigate to the directory `Http-Signal-Example` and open the visual studio solution `Http-Signal-Example.sln`
+
+3. Build both the TestApplication and the WebServer projects.
+
+4. Run the WebServer project.
+
+5. Run the TestApplication project.
+
+6. Observe the request via the web browser. ([http://localhost:5175/](http://localhost:5175/))
+
 ## License
 
 Distributed under the MIT License. See `LICENSE` file for more information.
