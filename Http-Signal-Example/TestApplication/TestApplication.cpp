85f040494dd6a3b93f57a8ca50131832b4cd4419
Rory Claasen <rory@xbox.com>
2022-07-13 10:15:40 +0100

    Adds more post examples

--- Http-Signal-Example/TestApplication/TestApplication.cpp
+++ Http-Signal-Example/TestApplication/TestApplication.cpp
@@ -1,15 +1,43 @@
-#include <httplib.h>
 #include <iostream>
+
+#include <httplib.h>
+
+void Post(std::string path, std::string data)
+{
+    try {
+        std::cout << "Post to " << path << "\n";
+
+        httplib::Client client("http://localhost:5175");
+
+        auto response = client.Post(path.c_str(), data, "application/json");
+        if (response.error() == httplib::Error::Success)
+        {
+            std::cout << "Post Response " << response->body << "\n";
+        }
+        else
+        {
+            std::cout << "Failed to send post to " << path << "\n";
+        }
+    }
+    catch (...)
+    {
+        std::cout << "Failed to send post to " << path << "\n";
+    }
+}
+
+std::string MakeMessage(std::string message)
+{
+    std::string title = "TestApplication";
+    return "{ \"title\": \"" + title + "\", \"message\": \"" + message + "\" }";
+}
 
 int main()
 {
-    httplib::Client client("http://localhost:5175");
+    Post("/data", MakeMessage("This is a blocking call"));
 
-    std::string path = "/crash";
-    std::string postBody = "{ \"title\": \"TestApplication\", \"message\": \"This is an example\" }";
+    std::thread start_thread([] { Post("/start", MakeMessage("This is the start of the program")); });
+    std::thread crash_thread([] { Post("/crash", MakeMessage("Oh no you crashed")); });
 
-    auto response = client.Post(path.c_str(), postBody, "application/json");
-
-    std::cout << "Response Status: " << response->status << "\n";
-    std::cout << "Response Body: " << response->body;
+    start_thread.join();
+    crash_thread.join();
 }
